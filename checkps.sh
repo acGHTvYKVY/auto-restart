cd50242b2e31e2605dfba91443cc8b92237f6eb5
acGHTvYKVY <113487230+acGHTvYKVY@users.noreply.github.com>
2025-07-27 22:30:57 +0800

    Update checkps.sh

--- checkps.sh
+++ checkps.sh
@@ -1,1 +1,13 @@
-
+#!/bin/bash
+while true; do
+PID=`ps aux | grep '/root/cysic-verifier/verifier' | grep -v grep | awk '{print $2}' | wc -l`
+echo $PID
+#if[ $PID="" ]; then
+if [ "$PID" = "0" ]; then
+  echo "cysic没启动"
+  /root/cysic-verifier/run.sh
+else
+  echo "cysic启动着"
+fi
+sleep 1800
+done
